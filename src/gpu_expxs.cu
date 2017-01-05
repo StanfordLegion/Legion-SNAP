c54b83fef71121b1997f43c421595e0c3e645046
Mike <mebauer@cs.stanford.edu>
2017-01-04 16:17:13 -0800

    adding contract attribution

--- src/gpu_expxs.cu
+++ src/gpu_expxs.cu
@@ -1,5 +1,7 @@
 /* Copyright 2017 NVIDIA Corporation
  *
+ * The U.S. Department of Energy funded the development of this software 
+ * under subcontract B609478 with Lawrence Livermore National Security, LLC
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
